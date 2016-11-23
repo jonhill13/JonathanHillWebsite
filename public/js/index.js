5adcc5b99c32c9355fca8a1dfcfd35c6177c34fe
Jonathan Hill <jonhill13@Jonathans-MBP.fios-router.home>
2016-11-22 23:13:26 -0500

    initial commit

---
+++ public/js/index.js
@@ -0,0 +1,50 @@
+//show resume container
+$('#chevronUp').on('click', function() {
+	$('#carouselDiv').animate({
+		top: 0,
+		height: 'toggle',
+	}, 1000, function() {
+
+	});
+	$('#resumeContainer').animate({
+		bottom: 0,
+		height: 'toggle',
+	}, 1000, function() {
+
+	});
+});
+//show intro container
+$('#chevronDown').on('click', function() {
+	$('#resumeContainer').animate({
+		bottom: 0,
+		height: 'toggle'
+	}, 1000, function() {
+
+	});
+	$('#carouselDiv').animate({
+		top: 0,
+		height: 'toggle'
+	}, 1000, function() {
+
+	});
+});
+//show soft quote page
+$('#softQuoteA').on('click', function () {
+	showCarouselPage(1);
+});
+//show operation dial out page
+$('#operationDialOutA').on('click', function () {
+	showCarouselPage(2);
+});
+//show call center page
+$('#callCenterA').on('click', function () {
+	showCarouselPage(3);
+});
+//show intro page
+$('.handLeft').on('click', function() {
+	showCarouselPage(0);
+});
+//show carousel page based on page number
+function showCarouselPage(pageNumber) {
+	$('.carousel').carousel(pageNumber);
+}
